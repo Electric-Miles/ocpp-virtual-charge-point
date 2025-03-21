e98ba1c5721b7e0b80b6e95fa4afc709be0ece4a
Chimezie Enyinnaya <meziemichael@gmail.com>
2025-03-21 13:12:29 +0100

    refactor: move charge point routes to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
 import "dotenv/config";
 import fastify, { FastifyReply, FastifyRequest } from "fastify";
-import {
-  changeVcpStatus,
-  getVcpStatus,
-  startVcp,
-  stopVcp,
-} from "./controllers/chargePointController";
-import {
-  ChangeVcpStatusValidationSchema,
-  StartVcpValidationSchema,
-  StatusValidationSchema,
-  StopVcpValidationSchema,
-} from "./schema";
 import fs from "fs";
 import { authRoutes } from "./routes/auth";
 import fastifyJwt from "@fastify/jwt";
 import fastifyAuth from "@fastify/auth";
+import { chargePointRoutes } from "./routes/charge-point";
 
 const app = fastify({
   logger: true,
@@ -39,51 +28,14 @@ app
   )
   .register(fastifyAuth)
   .after(() => {
-    app.post(
-      "/api/vcp/start",
-      {
-        schema: {
-          body: StartVcpValidationSchema,
-        },
-        preHandler: app.auth([app.verifyJwt]),
-      },
-      startVcp,
-    );
-    app.post(
-      "/api/vcp/stop",
-      { schema: { body: StopVcpValidationSchema } },
-      stopVcp,
-    );
-    app.get(
-      "/api/vcp/status",
-      {
-        schema: {
-          querystring: StatusValidationSchema,
-        },
-      },
-      getVcpStatus,
-    );
-    app.post(
-      "/api/vcp/change-status",
-      {
-        schema: {
-          body: ChangeVcpStatusValidationSchema,
-        },
-      },
-      changeVcpStatus,
-    );
-
+    app.register(chargePointRoutes, { prefix: "/api/vcp/" });
     app.register(authRoutes);
 
-    app.get(
-      "/control",
-      // { preHandler: app.auth([app.verifyJwt]) },
-      async (request, reply) => {
-        const stream = fs.readFileSync("./public/control.html").toString();
+    app.get("/control", async (request, reply) => {
+      const stream = fs.readFileSync("./public/control.html").toString();
 
-        reply.type("text/html").send(stream);
-      },
-    );
+      reply.type("text/html").send(stream);
+    });
   });
 
 const start = async () => {
