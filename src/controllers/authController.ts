c2cd40e153035a7ed97948f6494668beeb9c17de
alan jeeves <alan@electricmiles.co.uk>
2025-03-26 12:48:25 +0000

    add generic user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -5,6 +5,10 @@ const users = {
   password: process.env.USERS_PASSWORD,
   users: [
     {
+      first_name: "User",
+      email: "em",
+    },
+    {
       first_name: "Alan",
       email: "alan@electricmiles.co.uk",
     },
